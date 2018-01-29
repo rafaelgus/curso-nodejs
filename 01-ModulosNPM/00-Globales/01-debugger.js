81a6c436340dd942e53678e7d2b34b92151bda42
Benjamin Ocampo <benjamin@debian.link>
2018-01-29 17:26:29 -0300

    Instalamos paquetes globales del gestor de paquetes de Javascript, NPM. Vemos el debugger que trae la plataforma de Node.js, realizando un pequeño ejercicio con el mismo. Instalamos pm2, y con el archivo 02-monitor.js lo corremos de manera continua usando esta aplicación.

---
+++ 01-ModulosNPM/00-Globales/01-debugger.js
@@ -0,0 +1,35 @@
+'use strict'
+/*
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hola Mundo\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
+});
+*/
+
+const http = require('http')
+const hostname = 'localhost'
+const port = 3000
+
+function webServer (req,res)
+{
+	res.statusCode = 200
+	res.setHeader('Content-Type','text/html')
+	debugger;
+	res.end('<h1>Hola Mundo</h1>')
+}
+
+http
+	.createServer(webServer)
+	.listen(port,hostname)
+
+console.log('Servidor corriendo en http//localhost:3000/')
---
+++ 01-ModulosNPM/00-Globales/02-monitor.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const http = require('http').createServer(webServer),
+	fs = require('fs'),
+	port = 3000
+
+function webServer(req,res)
+{
+	function readFile(err,data)
+	{
+		if(err) throw err
+		res.end(data)
+	}
+	res.writeHead(200,{'Content-Type':'text/html'})
+	fs.readFile('assets/index.html',readFile)
+}
+
+http.listen(port)
+
+console.log('Servidor corriendo en http://localhost:3000')
